61dd324265ae6d8eab5a8f361e790a2e6cc29005
Oz Blumenfeld <ozb077@gmail.com>
2026-05-24 01:44:10 +0300

    fix 2

--- oz_shared/__init__.py
+++ oz_shared/__init__.py
@@ -1,7 +1,7 @@
-from oz_shared.onepassword import load_op_secrets, load_op_secrets_sdk
+from oz_shared.onepassword import load_op_secrets
 from oz_shared.types import OptStr
 
-__all__ = ["OptStr", "load_op_secrets", "load_op_secrets_sdk"]
+__all__ = ["OptStr", "load_op_secrets_deprecated", "load_op_secrets"]
 
 try:
     from oz_shared.postgres import make_engine, make_get_session, make_session_factory
